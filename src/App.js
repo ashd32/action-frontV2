8980ddf930388969b8445e6928db36c5542bfc0d
Ashley Donald <ashd32@gmail.com>
2019-11-30 21:24:23 -0500

    inception

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import './App.css';
+
+import Landing from "./Landing";
+import Home from "./Home";
+import Devices from "./Devices";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route render={({ location, history }) => (
+            <React.Fragment>
+              <SideNav
+                  onSelect={(selected) => {
+                    const to = '/' + selected;
+                    if (location.pathname !== to) {
+                      history.push(to);
+                    }
+                  }}
+              >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                  <NavItem eventKey="home">
+                    <NavIcon>
+                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                    <NavText>
+                      Home
+                    </NavText>
+                  </NavItem>
+                  <NavItem eventKey="devices">
+                    <NavIcon>
+                      <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                    <NavText>
+                      Devices
+                    </NavText>
+                  </NavItem>
+                </SideNav.Nav>
+              </SideNav>
+              <main>
+                <Route path="/" exact component={props => <Landing />} />
+                <Route path="/home" component={props => <Home />} />
+                <Route path="/devices" component={props => <Devices />} />
+              </main>
+            </React.Fragment>
+        )}
+        />
+      </Router>
     </div>
   );
 }
---
+++ src/Devices.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Devices(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+export default Devices;
